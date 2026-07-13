1b693c267599d067d6ab988adb20193e9a327a23
shun417jp <s.tomaki.sys23@morijyobi.ac.jp>
2026-07-13 12:13:55 +0900

    feat: add EC2 instance resource and update VPC Name tag

--- terraform-study/main.tf
+++ terraform-study/main.tf
@@ -16,6 +16,15 @@ provider "aws" {
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "hello"
+    Name = "Hello Morijyobi !!!!"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
   }
 }
