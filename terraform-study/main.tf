d82a9099973d386d66f4587b689789990a11616b
shun417jp <s.tomaki.sys23@morijyobi.ac.jp>
2026-07-13 11:32:02 +0900

    feat: configure AWS provider and add VPC resource in main.tf

--- terraform-study/main.tf
+++ terraform-study/main.tf
@@ -1,13 +1,18 @@
 terraform {
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
 
-resource "local_file" "hello" {
-  filename = "${path.module}/hello.txt"
-  content  = "Hello, Terraform! This file was created by Terraform."
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
